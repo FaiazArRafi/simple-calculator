39c856d3678be3d7db4230a5109530fe79b4ec6d
Faiaz <faiazarrafi@gmail.com>
2023-05-14 19:51:16 +0600

    feat: nubmers show and clear
    numbers showes on display by clicking on them and get cleard when the clear button get clicked on

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('adsk');
+console.log('adskg');
+
+const inputField = document.getElementById('input-field');
+
+function showOnDisplay(value) {
+    inputField.value += value;
+}
+
+document.getElementById('del').addEventListener('click', function () {
+    inputField.value = '';
+})
